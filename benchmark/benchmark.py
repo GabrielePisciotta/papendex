e99c86ad0440116961ff6c6e2b23ee0fe4bdf030
Gabriele <ga.pisciotta@gmail.com>
2020-08-27 19:42:37 +0200

    Benchmark with query_be

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.utils import shuffle
 import time
 import pysolr
+import re
 import requests
 from urllib.parse import quote
 
@@ -101,8 +102,55 @@ def crossref_query(filename='benchmark/benchmark_queries.csv'):
     df_crossref.to_csv('benchmark/benchmark_queries_results_crossref.csv', index=False, encoding='utf-8', sep=';')
     print("Time elapsed for queries: {:.3f}s".format((end-start)))
 
+def run_benchmark_2(filename='benchmark/benchmark_queries_from_ccc.csv'):
+    df = shuffle(pd.read_csv(filename, sep=','))
+    solr = pysolr.Solr('http://localhost:8983/solr/crossref_without_metadata', always_commit=False, timeout=100)
+
+    accuracy = 0
+
+    print("Running benchmark")
+
+    start = time.time()
+    for _, row in df.iterrows():
+
+
+        str = row['query_be']
+        #str = re.sub(r'^https?:\/\/.*[\r\n]*', '', str)
+        str = ' '.join(item for item in str.split() if not (item.startswith('https://') and len(item) > 7))
+        str = ' '.join(item for item in str.split() if not (item.startswith('http://') and len(item) > 7))
+        str = str.replace("et al.", "")
+        str = str.replace("Available at:", "")
+        str = re.sub('\W+', ' ', str)
+        str = str.strip()
+
+        query = 'bibref:({})'.format(str)
+        results = solr.search(fl='*,score', q=query)
+
+        if len(results) < 1:
+            print("[len = {}] Error with {}".format(len(results),query))
+            continue
+
+        r_dois = [r['id'] for r in results]
+        if row['retrieved_doi'] not in r_dois:
+            print("Results don't contain proper doi.")
+            print("Query: {}".format(str))
+            print("Query orig: {}".format(row['query_be']))
+            r = "Results: \n"
+            r = '\n'.join([x['bibref'][0] for x in results])
+            print(r+"\n\n")
+        else:
+            accuracy += 1
+
+    end = time.time()
+
+    print("Accuracy: {}".format((accuracy)*100/len(df.index)))
+
+    print("Total time for bibref queries: {:.3f}s".format((end-start)))
+    print("Mean time elapsed for a bibref query: {:.3f}s".format((end-start)/1000))
+
+
 if __name__ == '__main__':
     #create_dataset()
-    run_benchmark()
+    run_benchmark_2()
     #crossref_query()
 
